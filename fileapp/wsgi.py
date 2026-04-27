c57d5d99056757413d50b2bfdf05b04c13a522e1
Temitope <tpsolesi@gmail.com>
2026-04-28 00:00:13 +0100

    Update wsgi.py
    update wsgi

--- fileapp/wsgi.py
+++ fileapp/wsgi.py
@@ -1,13 +1,10 @@
-"""
-WSGI config for fileapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
+
+# Add the project root directory to the python path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
